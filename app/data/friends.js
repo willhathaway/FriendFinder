503b9e4fd5650747538470ca43319016236ebc80
Will Hathaway <william.hathaway38@gmail.com>
2019-12-19 10:39:28 -0500

    templates copied/modified from HotRestaurant activity

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+let Will = {
+    name: "Will",
+    photo: "https://media.licdn.com/dms/image/C4D03AQGGYQdCbzENOQ/profile-displayphoto-shrink_200_200/0?e=1582156800&v=beta&t=Zln4iOV_tz9ijvGj2ueyg8AeGEvx0ecVXfXshUNXoO4",
+    scores: [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+}
